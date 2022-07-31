8627868908bd82d64e05d66e9f3b08bf94142ebd
Linh Nguyen <xiahii123@gmail.com>
2022-08-01 00:31:14 +0700

    Commit bai 13

---
+++ src/webdriver/Topic_02_XPath_PartI.java
@@ -0,0 +1,76 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_XPath_PartI {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe"); //trỏ đến file geckodriver.exe
+		driver = new FirefoxDriver(); //library
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void TC_01_() {
+		driver.get("https://www.facebook.com/");
+		
+		/*<input class="text form-control" 
+		 * id="txtEmail" name="txtEmail" 
+		 * placeholder="Địa chỉ email" type="email" 
+		 * value="" aria-required="true" aria-invalid="true">*/
+		
+		
+		// 1 - < hoặc <> : thẻ mở
+		// 2 - tên thẻ (Tagname): input/ html/ body/ head/ form/ lable/ span/ p/...
+		// 3 - thuộc tính (Attribute name): class/ id/ name/ placeholder/ type/ value
+		// 4 - giá trị của thuộc tính (Attribute value): text form-control/ txtEmail/... 
+		// 5 - > hoặc </> : thẻ đóng
+		
+		// tagname - Attribute name - Attribute value
+		
+		// XPath Format Basic
+		// Absolute XPath: /html/body/...
+		// Relative XPath: //tagname[@attribute-name='attribute-value']
+		// Relative XPath: //tagname[@attribute-name="attribute-value"]
+		
+		// CSS Format Basic
+		// tagname[attribute-name='attribute-value']
+		// tagname[attribute-name="attribute-value"]
+	
+		// Locator code gọn hơn
+		driver.findElement(By.xpath("//input[@id='Password']"));
+		// Locator code xấu hơn - khó đọc hơn
+		driver.findElement(By.xpath("//*[@id=\"Password\"]"));
+	
+	}
+
+	@Test
+	public void TC_02_ValidatePageTitle() {
+		// Login Page title
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
+	}
+
+	@Test
+	public void TC_03_LoginFormDisplayed() {
+		// Login form displayed
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
