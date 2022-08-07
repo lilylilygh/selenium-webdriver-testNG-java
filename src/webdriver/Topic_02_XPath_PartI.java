78bc94b393774bf00b2d967f807c4b9d1a75ee02
Linh Nguyen <xiahii123@gmail.com>
2022-08-07 18:08:29 +0700

    Commit bài 16 - XPath

--- src/webdriver/Topic_02_XPath_PartI.java
+++ src/webdriver/Topic_02_XPath_PartI.java
@@ -13,60 +13,122 @@ import org.testng.annotations.Test;
 public class Topic_02_XPath_PartI {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
 
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe"); //trỏ đến file geckodriver.exe
-		driver = new FirefoxDriver(); //library
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe"); 
+		driver = new FirefoxDriver(); 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
 	}
-
+		
 	@Test
-	public void TC_01_() {
-		driver.get("https://www.facebook.com/");
+	public void Register_01_Empty_Data() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
 		
-		/*<input class="text form-control" 
-		 * id="txtEmail" name="txtEmail" 
-		 * placeholder="Địa chỉ email" type="email" 
-		 * value="" aria-required="true" aria-invalid="true">*/
+		// Action
+		driver.findElement(By.id("txtFirstname")).sendKeys("");
+		driver.findElement(By.id("txtEmail")).sendKeys("");
+		driver.findElement(By.id("txtCEmail")).sendKeys("");
+		driver.findElement(By.id("txtPassword")).sendKeys("");
+		driver.findElement(By.id("txtCPassword")).sendKeys("");
+		driver.findElement(By.id("txtPhone")).sendKeys("");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
 		
-		
-		// 1 - < hoặc <> : thẻ mở
-		// 2 - tên thẻ (Tagname): input/ html/ body/ head/ form/ lable/ span/ p/...
-		// 3 - thuộc tính (Attribute name): class/ id/ name/ placeholder/ type/ value
-		// 4 - giá trị của thuộc tính (Attribute value): text form-control/ txtEmail/... 
-		// 5 - > hoặc </> : thẻ đóng
-		
-		// tagname - Attribute name - Attribute value
-		
-		// XPath Format Basic
-		// Absolute XPath: /html/body/...
-		// Relative XPath: //tagname[@attribute-name='attribute-value']
-		// Relative XPath: //tagname[@attribute-name="attribute-value"]
-		
-		// CSS Format Basic
-		// tagname[attribute-name='attribute-value']
-		// tagname[attribute-name="attribute-value"]
+		// Verify (Actual data = Expect data)
+		Assert.assertEquals(driver.findElement(By.id("txtFirstname-error")).getText(), "Vui lòng nhập họ tên");
+		Assert.assertEquals(driver.findElement(By.id("txtEmail-error")).getText(), "Vui lòng nhập email");
+		Assert.assertEquals(driver.findElement(By.id("txtCEmail-error")).getText(), "Vui lòng nhập lại địa chỉ email");
+		Assert.assertEquals(driver.findElement(By.id("txtPassword-error")).getText(), "Vui lòng nhập mật khẩu");
+		Assert.assertEquals(driver.findElement(By.id("txtCPassword-error")).getText(), "Vui lòng nhập lại mật khẩu");
+		Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(), "Vui lòng nhập số điện thoại.");
+	}
 	
-		// Locator code gọn hơn
-		driver.findElement(By.xpath("//input[@id='Password']"));
-		// Locator code xấu hơn - khó đọc hơn
-		driver.findElement(By.xpath("//*[@id=\"Password\"]"));
 	
-	}
-
 	@Test
-	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
+	public void Register_02_Invalid_Email() {
+		// Action
+		driver.findElement(By.id("txtFirstname")).sendKeys("Linh");
+		driver.findElement(By.id("txtEmail")).sendKeys("123@123@456");
+		driver.findElement(By.id("txtCEmail")).sendKeys("123@123@456");
+		driver.findElement(By.id("txtPassword")).sendKeys("123456");
+		driver.findElement(By.id("txtCPassword")).sendKeys("123456");
+		driver.findElement(By.id("txtPhone")).sendKeys("0987654321");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+		
+		// Verify (Actual data = Expect data)
+		Assert.assertEquals(driver.findElement(By.id("txtEmail-error")).getText(), "Vui lòng nhập email hợp lệ");
+		
+		// Bug
+		Assert.assertEquals(driver.findElement(By.id("txtCEmail-error")).getText(), "Vui lòng nhập email hợp lệ");
 	}
-
+	
 	@Test
-	public void TC_03_LoginFormDisplayed() {
-		// Login form displayed
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+	public void Register_03_Incorrect_Email() {
+		driver.findElement(By.id("txtFirstname")).sendKeys("Linh");
+		driver.findElement(By.id("txtEmail")).sendKeys("linh@gmail.com");
+		driver.findElement(By.id("txtCEmail")).sendKeys("linh@hotmail.net");
+		driver.findElement(By.id("txtPassword")).sendKeys("123456");
+		driver.findElement(By.id("txtCPassword")).sendKeys("123456");
+		driver.findElement(By.id("txtPhone")).sendKeys("0987654321");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+		
+		// Verify (Actual data = Expect data)
+		Assert.assertEquals(driver.findElement(By.id("txtCEmail-error")).getText(), "Email nhập lại không đúng");
+	}
+	
+	@Test
+	public void Register_04_Password_Less_Than_6_Chars() {
+		driver.findElement(By.id("txtFirstname")).sendKeys("Linh");
+		driver.findElement(By.id("txtEmail")).sendKeys("linh@gmail.com");
+		driver.findElement(By.id("txtCEmail")).sendKeys("linh@gmail.com");
+		driver.findElement(By.id("txtPassword")).sendKeys("12345");
+		driver.findElement(By.id("txtCPassword")).sendKeys("123456");
+		driver.findElement(By.id("txtPhone")).sendKeys("0987654321");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+		
+		// Verify (Actual data = Expect data)
+		Assert.assertEquals(driver.findElement(By.id("txtPassword-error")).getText(), "Mật khẩu phải có ít nhất 6 ký tự");
+	}
+	
+	@Test
+	public void Register_05_Incorrect_Confirm_Password() {
+		driver.findElement(By.id("txtFirstname")).sendKeys("Linh");
+		driver.findElement(By.id("txtEmail")).sendKeys("linh@gmail.com");
+		driver.findElement(By.id("txtCEmail")).sendKeys("linh@gmail.com");
+		driver.findElement(By.id("txtPassword")).sendKeys("123456");
+		driver.findElement(By.id("txtCPassword")).sendKeys("1234567");
+		driver.findElement(By.id("txtPhone")).sendKeys("0987654321");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+		
+		// Verify (Actual data = Expect data)
+		Assert.assertEquals(driver.findElement(By.id("txtCPassword-error")).getText(), "Mật khẩu bạn nhập không khớp");
+	}
+	
+	@Test
+	public void Register_06_Invalid_Phone_Number() {
+		driver.findElement(By.id("txtFirstname")).sendKeys("Linh");
+		driver.findElement(By.id("txtEmail")).sendKeys("linh@gmail.com");
+		driver.findElement(By.id("txtCEmail")).sendKeys("linh@gmail.com");
+		driver.findElement(By.id("txtPassword")).sendKeys("123456");
+		driver.findElement(By.id("txtCPassword")).sendKeys("123456");
+		
+		// <10 chars
+		driver.findElement(By.id("txtPhone")).sendKeys("0987");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+		Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(), "Số điện thoại phải từ 10-11 số.");
+		
+		
+		// >11 chars
+		driver.findElement(By.id("txtPhone")).sendKeys("0987164646464364164");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+		Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(), "Số điện thoại phải từ 10-11 số.");
+		
+		// Start without 0 number
+		driver.findElement(By.id("txtPhone")).sendKeys("987164644");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+		Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(), "Số điện thoại bắt đầu bằng: 09 - 03 - 012 - 016 - 018 - 019");
+		
 	}
 
 	@AfterClass
--- src/webdriver/Topic_03_Textbox.java
+++ src/webdriver/Topic_03_Textbox.java
@@ -3,6 +3,13 @@ package webdriver;
 public class Topic_03_Textbox {
 
 	public void clickToLoginButton () {
+	}
+	
+	public static void main(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
 		
+		String osName = System.getProperty("os.name");
+		System.out.println(osName);
 	}
 }
