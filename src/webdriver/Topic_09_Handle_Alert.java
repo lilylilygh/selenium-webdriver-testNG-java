75cb75745e216b0d9addee99ab8c72b509b0763d
Linh Nguyen <xiahii123@gmail.com>
2022-12-05 03:06:53 +0700

    Alert

--- src/webdriver/Topic_09_Handle_Alert.java
+++ src/webdriver/Topic_09_Handle_Alert.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -71,6 +72,70 @@ public class Topic_09_Handle_Alert {
 		System.out.println("Hexa color = " + hexaColor);
 		// Verify background color
 		Assert.assertEquals(hexaColor, "#C92127");
+	}
+	
+	@Test
+	public void TC_02_Confirm_Alert() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		
+		alert = driver.switchTo().alert();
+		Assert.assertEquals(alert.getText(), "I am a JS Confirm");
+		alert.dismiss();
+		Assert.assertEquals(driver.findElement(By.cssSelector("p#result")).getText(), "You clicked: Cancel");
+	}
+	
+//	@Test
+	public void TC_03_Prompt_Alert() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		
+		alert = driver.switchTo().alert();
+		Assert.assertEquals(alert.getText(), "I am a JS prompt");
+		String promptMessage = "Welcome to Prompt Alert";
+		alert.sendKeys(promptMessage);
+		alert.accept();
+//		Assert.assertEquals(driver.findElement(By.cssSelector("p#result")), "You entered: "+ promptMessage);
+		Assert.assertTrue(driver.findElement(By.cssSelector("p#result")).getText().contains(promptMessage), promptMessage);
+		
+		
+	}
+//	@Test
+	public void TC_04_Authentication_Alert() {
+//		http://the-internet.herokuapp.com/basic_auth
+		String url = authenticationUrl("admin", "admin", "http://the-internet.herokuapp.com/basic_auth");
+		driver.get(url);
+		sleepInSecond(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
+	}
+//	@Test
+	public void TC_05_Authentication_Alert_Navigate_From_Other_Page() {
+//		http://the-internet.herokuapp.com/basic_auth
+//		String originUrl = "http://the-internet.herokuapp.com/";
+		driver.get("http://the-internet.herokuapp.com/");
+		String newUrl = driver.findElement(By.xpath("//a[text()='Basic Auth']")).getAttribute("href");
+		System.out.println(newUrl);
+		String url = authenticationUrl("admin", "admin", newUrl);
+		driver.get(url);
+		sleepInSecond(3);
+		Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
+	}
+	@Test
+	public void TC_06_Authentication_Alert_AutoIT() throws IOException {
+//		http://the-internet.herokuapp.com/basic_auth
+		String userName = "admin";
+		String passWord = "admin";
+		Runtime.getRuntime().exec(new String[] {autoITFireFox,userName,passWord}); 	
+		
+		driver.get("http://the-internet.herokuapp.com/basic_auth");
+		sleepInSecond(20);
+		Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
+	}
+	public String authenticationUrl (String userName, String passWord, String url) {
+		String[] split = url.split("//");
+//		format: https:/admin:password@url
+		String authenticationUrl = split[0] + userName + ":" + passWord + "@" + split[1];
+		return authenticationUrl;
 	}
 	
 	@AfterClass
