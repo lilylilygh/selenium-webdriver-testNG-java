4933481728cbd7ada0974142ac3d6a86a2d371ae
Linh Nguyen <xiahii123@gmail.com>
2022-12-24 23:53:20 +0700

    JS Executor

--- src/webdriver/Topic_14_Javascript_Executer.java
+++ src/webdriver/Topic_14_Javascript_Executer.java
@@ -20,58 +20,62 @@ public class Topic_14_Javascript_Executer {
 
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe"); //trỏ đến file geckodriver.exe
-		driver = new FirefoxDriver(); //library
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe"); // trỏ đến file
+																											// geckodriver.exe
+		driver = new FirefoxDriver(); // library
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		//driver.get("https://www.facebook.com/");
-		
-		// Vs 1 interface: add kiểu của driver qua jsExecutor, interface k dùng khởi tạo, k có new
+		// driver.get("https://www.facebook.com/");
+
+		// Vs 1 interface: add kiểu của driver qua jsExecutor, interface k dùng khởi
+		// tạo, k có new
 		// js được khởi tạo gắn driver vào -> gọi đc
 		jsExecutor = (JavascriptExecutor) driver;
 	}
 
 	@Test
 	public void TC_01_TechPanda() {
-		/* Note: Các đoạn Javascript vào ts chạy rất nhanh
-		 * Khi chuyển trang (navigate và click) trên các browser
-		 * nó sẽ k chờ các element đc ready trc khi thao tác
-		 * -> Nên có time sleep 
+		/*
+		 * Note: Các đoạn Javascript vào ts chạy rất nhanh Khi chuyển trang (navigate và
+		 * click) trên các browser nó sẽ k chờ các element đc ready trc khi thao tác ->
+		 * Nên có time sleep
 		 */
 		navigateToUrlByJS("http://live.techpanda.org/");
 		sleepInSecond(3);
-		String  techPandaDomain = (String) executeForBrowser(" return document.domain");
+		String techPandaDomain = (String) executeForBrowser(" return document.domain");
 		Assert.assertEquals(techPandaDomain, "live.techpanda.org");
-		
-		String  homePageURL = (String) executeForBrowser(" return document.URL");
+
+		String homePageURL = (String) executeForBrowser(" return document.URL");
 		Assert.assertEquals(homePageURL, "http://live.techpanda.org/");
-		
+
 		hightlightElement("//a[text()='Mobile']");
 		clickToElementByJS("//a[text()='Mobile']");
 		sleepInSecond(3);
-		
-		hightlightElement("//a[text()='Samsung Galaxy']/parent::h2/following-sibling::div/button[@title='Add to Cart']");
-		clickToElementByJS("//a[text()='Samsung Galaxy']/parent::h2/following-sibling::div/button[@title='Add to Cart']");
+
+		hightlightElement(
+				"//a[text()='Samsung Galaxy']/parent::h2/following-sibling::div/button[@title='Add to Cart']");
+		clickToElementByJS(
+				"//a[text()='Samsung Galaxy']/parent::h2/following-sibling::div/button[@title='Add to Cart']");
 		sleepInSecond(3);
 		Assert.assertTrue(areExpectedTextInInnerText("Samsung Galaxy was added to your shopping cart."));
-		
+
 		hightlightElement("//a[text()='Customer Service']");
 		clickToElementByJS("//a[text()='Customer Service']");
 		sleepInSecond(3);
-		
+
 		hightlightElement("//input[@id='newsletter']");
 		scrollToElementOnTop("//input[@id='newsletter']");
-		
+
 		String emailAddrress = "lily" + generateRandomNumber() + "@hotmail.vn";
-		sendkeyToElementByJS("//input[@id='newsletter']",emailAddrress);
-		
+		sendkeyToElementByJS("//input[@id='newsletter']", emailAddrress);
+
 		clickToElementByJS("//button[@title='Subscribe']");
 		sleepInSecond(3);
 		Assert.assertTrue(areExpectedTextInInnerText("Thank you for your subscription."));
 
 		navigateToUrlByJS("http://demo.guru99.com/v4/");
 		sleepInSecond(3);
-		String domainPageGuru  = (String) executeForBrowser("return document.domain");
+		String domainPageGuru = (String) executeForBrowser("return document.domain");
 		Assert.assertEquals(domainPageGuru, "demo.guru99.com");
 	}
 
@@ -79,32 +83,33 @@ public class Topic_14_Javascript_Executer {
 	public void TC_02_HTML5() {
 		navigateToUrlByJS("https://sieuthimaymocthietbi.com/account/register");
 		sleepInSecond(5);
-		
-		clickToElementByJS("//button[text()='Đăng ký']");	
+
+		clickToElementByJS("//button[text()='Đăng ký']");
 		String validationMessage = getElementValidationMessage("//input[@id='lastName']");
 		Assert.assertEquals(validationMessage, "Please fill out this field.");
-		sendkeyToElementByJS("//input[@id='lastName']","Li");
-		
-		clickToElementByJS("//button[text()='Đăng ký']");	
+		sendkeyToElementByJS("//input[@id='lastName']", "Li");
+
+		clickToElementByJS("//button[text()='Đăng ký']");
 		validationMessage = getElementValidationMessage("//input[@id='firstName']");
 		Assert.assertEquals(validationMessage, "Please fill out this field.");
-		sendkeyToElementByJS("//input[@id='firstName']","Ly");
-		
-		clickToElementByJS("//button[text()='Đăng ký']");	
+		sendkeyToElementByJS("//input[@id='firstName']", "Ly");
+
+		clickToElementByJS("//button[text()='Đăng ký']");
 		validationMessage = getElementValidationMessage("//input[@id='email']");
 		Assert.assertEquals(validationMessage, "Please fill out this field.");
-		sendkeyToElementByJS("//input[@id='email']","lily@hotmail.com");
-		
-		clickToElementByJS("//button[text()='Đăng ký']");	
+		sendkeyToElementByJS("//input[@id='email']", "lily@hotmail.com");
+
+		clickToElementByJS("//button[text()='Đăng ký']");
 		validationMessage = getElementValidationMessage("//input[@id='password']");
 		Assert.assertEquals(validationMessage, "Please fill out this field.");
-		sendkeyToElementByJS("//input[@id='password']","lily@hotmail.com");
+		sendkeyToElementByJS("//input[@id='password']", "lily@hotmail.com");
 	}
 
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
+
 	public void sleepInSecond(long timeInSecond) {
 		try {
 			Thread.sleep(timeInSecond * 1000); // Hàm java
@@ -112,7 +117,7 @@ public class Topic_14_Javascript_Executer {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// Những hàm này k phải của Selenium, mà chỉ là hàm của Javascript
 	// Hàm WebBrowser execute
 	public Object executeForBrowser(String javaScript) {
@@ -126,11 +131,11 @@ public class Topic_14_Javascript_Executer {
 
 	// Hàm kiểm tra 1 text có khớp với text truyền vào k?
 	public boolean areExpectedTextInInnerText(String textExpected) {
-		String textActual = (String) jsExecutor.executeScript("return document.documentElement.innerText.match('" + textExpected + "')[0];");
+		String textActual = (String) jsExecutor
+				.executeScript("return document.documentElement.innerText.match('" + textExpected + "')[0];");
 		return textActual.equals(textExpected);
 	}
 
-	
 	public void scrollToBottomPage() {
 		jsExecutor.executeScript("window.scrollBy(0,document.body.scrollHeight)");
 	}
@@ -143,7 +148,8 @@ public class Topic_14_Javascript_Executer {
 	public void hightlightElement(String locator) {
 		WebElement element = getElement(locator);
 		String originalStyle = element.getAttribute("style");
-		jsExecutor.executeScript("arguments[0].setAttribute('style', arguments[1])", element, "border: 2px solid red; border-style: dashed;");
+		jsExecutor.executeScript("arguments[0].setAttribute('style', arguments[1])", element,
+				"border: 2px solid red; border-style: dashed;");
 		sleepInSecond(1);
 		jsExecutor.executeScript("arguments[0].setAttribute('style', arguments[1])", element, originalStyle);
 	}
@@ -182,7 +188,7 @@ public class Topic_14_Javascript_Executer {
 	public WebElement getElement(String locator) {
 		return driver.findElement(By.xpath(locator));
 	}
-	
+
 	public int generateRandomNumber() {
 		Random rand = new Random();
 		return rand.nextInt(9999);
